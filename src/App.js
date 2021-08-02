022601c7dbd928954243875a20c5febf52188541
osiyeon <osiyun0313@gmail.com>
2021-08-02 18:01:06 +0900

    start code

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
-import Welcome from "./pages/Welcome";
-import Products from "./pages/Products";
-import ProductDetail from "./pages/ProductDetail";
-import MainHeader from "./components/MainHeader";
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
 
 function App() {
     return (
-        <div>
-            <MainHeader />
-            <main>
-                <Switch>
-                    <Route path="/" exact>
-                        <Redirect to="/welcome" />
-                    </Route>
-                    <Route path="/welcome">
-                        <Welcome />
-                    </Route>
-                    <Route path="/products" exact>
-                        <Products />
-                    </Route>
-                    <Route path="/products/:productId">
-                        <ProductDetail />
-                    </Route>
-                </Switch>
-            </main>
-        </div>
+        <Switch>
+            <Route path="/quotes" exact>
+                <AllQuotes />
+            </Route>
+            <Route path="/quotes/:quoteId">
+                <QuoteDetail />
+            </Route>
+            <Route path="/new-quote">
+                <NewQuote />
+            </Route>
+        </Switch>
     );
 }
 
 export default App;
-
-// our-domain.com/ => Component A
-// our-domain.com/products => Component B
-
-// our-domain.com/welcome url을 가지면 위에 <Route path="/welcome" /> 실행됨
-// our-domain.com/products => Products Component
-// our-domain.com/product-detail/<any-value>
